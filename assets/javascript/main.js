8af2d9b7ac39337b3581891793b89db3918c6fba
Paraprax <parkerjohnston28@gmail.com>
2020-09-02 05:42:46 -0400

    user inputs are now assigned to variables via jQ; outlined resultsPrinter function for updating the DOM later

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
 
-    //assign on-click event to 'search' button:
+    //initial vars to hold our user inputs:
+    let searchTerm = "";
+    let numberOfResults = 0;
+
+    //on-click event for 'search' button:
     $('#search-button').on('click', function() {
         console.log('searching');
+
+        //assign user inputs to vars:
+        searchTerm = $('#search-term').val();
+        numberOfResults = $('#num-of-results').val();
+
+
+        //finally print the results to the DOM:
+        //resultsPrinter(results);
     });
-    
+
+    //function for updating the DOM with the formatted search results:
+    const resultsPrinter = (news) => {
+        //$('#results-card').html(news);
+        console.log();
+    }
+
 //end of docready function.
 });
