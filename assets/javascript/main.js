316c4cb0890dbacfcb34d5a843a65fada209701f
Paraprax <parkerjohnston28@gmail.com>
2020-09-05 00:54:02 -0400

    debugging authorization issue; result - 's' missing from  https of handwritten query URL

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,14 +4,28 @@ $(document).ready(function() {
     let searchTerm = "";
     let numberOfResults = 0;
 
+    //NYT APIkey:
+    const APIkey = '3BIqflRfl3yifYGKnHsGmykzEwPzZAgf';
+
     //on-click event for 'search' button:
     $('#search-button').on('click', function() {
         console.log('searching');
+        console.log(APIkey.length);
 
         //assign user inputs to vars:
         searchTerm = $('#search-term').val();
         numberOfResults = $('#num-of-results').val();
 
+        //build the query URL using the two input vars && our API key:
+        let queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTerm}&api-key=${APIkey}`;
+
+        //make the AJAX call to the NYT's API:
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function(response) {
+            console.log(response);
+        });
 
         //finally print the results to the DOM:
         //resultsPrinter(results);
